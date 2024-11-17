fac1eea5746819a3a413c3f92ca4b0bc366acd0c
JohnnyJones <c18jonathan.gale@gmail.com>
2024-11-17 18:06:03 -0500

    basic project layout

---
+++ frontend/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Example from '../components/example.tsx'
+
+function App() {
+    return(
+        <>
+            <p>APP PAGE!</p>
+            <Example />
+        </>
+    )
+}
+
+export default App;
---
+++ frontend/components/example.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Example() {
+    return (
+        <>
+          <p>THIS IS AN EXAMPLE COMPOENT.</p>
+        </>
+    )
+}
+
+export default Example;
