63ac26a0b86649d9f6b947363317b95b246304f9
JohnnyJones <c18jonathan.gale@gmail.com>
2024-11-19 00:50:52 -0500

    static gradient bg

--- frontend/app/main.tsx
+++ frontend/app/main.tsx
@@ -3,11 +3,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import Nav from "../components/Nav.tsx";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="bg-gradient-to-t from-breezy to-white dark:to-gray-900 fixed inset-0 -z-10 mt-16"></div>
+    <div className="bg-gradient-to-t from-breezy to-80% to-white dark:to-gray-900 fixed inset-0 -z-10"></div>
     <Nav />
-    <div className="w-max container mx-auto p-4 justify-center">
+    <div className="w-max container mx-auto p-4 justify-center mt-16">
       <App />
     </div>
   </StrictMode>
