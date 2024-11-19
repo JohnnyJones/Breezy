996ee297121ae85f113c84aae4d961d3087728f6
JohnnyJones <c18jonathan.gale@gmail.com>
2024-11-18 22:48:28 -0500

    fix bg changing color and size as content shifts

--- frontend/app/main.tsx
+++ frontend/app/main.tsx
@@ -3,14 +3,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import Nav from "../components/Nav.tsx";
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <div className="bg-gradient-to-t from-breezy to-white dark:to-gray-900 fixed inset-0 -z-10 mt-16"></div>
     <Nav />
-    <div className="bg-gradient-to-t from-breezy to-white dark:to-gray-900">
-      <div className="container mx-auto p-4">
-        <App />
-      </div>
+    <div className="w-max container mx-auto p-4 justify-center">
+      <App />
     </div>
   </StrictMode>
 );
